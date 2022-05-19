bf3c3a16d7f40fe058ba0ee7d22fd77e2d61d47c
Adam Kelly <adam@cthulahoops.org>
2022-05-19 18:11:51 +0100

    Reorder the code.

--- clock.py
+++ clock.py
@@ -9,34 +9,6 @@ MIDPOINT = (CLOCK_SIZE - 1) / 2
 hat = SenseHat()
 hat.clear()
 
-def pixel_angle_delta(pixel, angle):
-    simple_angle = abs(math.atan2(pixel[1] - MIDPOINT, pixel[0] - MIDPOINT) % (2 * math.pi) - angle)
-    return min(simple_angle, 2 * math.pi - simple_angle)
-
-def pixel_closest_to_angle(pixels, angle):
-    return min(pixels, key=lambda pos: pixel_angle_delta(pos, angle))
-
-def clamp(value, min_value, max_value):
-    return min(max(value, min_value), max_value)
-
-def clamp_rgb(rgb):
-    return [clamp(int(rgb[0]), 0, 255), clamp(int(rgb[1]), 0, 255), clamp(int(rgb[2]), 0, 255)]
-
-class Buffer:
-    DISPLAY_SIZE = 8
-    def __init__(self):
-        self.buffer = [[0, 0, 0] for x in range(64)]
-
-    def __setitem__(self, key, value):
-        self.buffer[key[1] * self.DISPLAY_SIZE + key[0]] = clamp_rgb(value)
-
-    def __getitem__(self, key):
-        return self.buffer[key[1] * self.DISPLAY_SIZE + key[0]]
-
-    def draw(self):
-        hat.set_pixels(self.buffer)
-
-
 def main():
     while True:
         buffer = Buffer()
@@ -66,6 +38,34 @@ def main():
         time.sleep(0.5)
 
         buffer.draw()
+
+def pixel_angle_delta(pixel, angle):
+    simple_angle = abs(math.atan2(pixel[1] - MIDPOINT, pixel[0] - MIDPOINT) % (2 * math.pi) - angle)
+    return min(simple_angle, 2 * math.pi - simple_angle)
+
+def pixel_closest_to_angle(pixels, angle):
+    return min(pixels, key=lambda pos: pixel_angle_delta(pos, angle))
+
+def clamp(value, min_value, max_value):
+    return min(max(value, min_value), max_value)
+
+def clamp_rgb(rgb):
+    return [clamp(int(rgb[0]), 0, 255), clamp(int(rgb[1]), 0, 255), clamp(int(rgb[2]), 0, 255)]
+
+class Buffer:
+    DISPLAY_SIZE = 8
+    def __init__(self):
+        self.buffer = [[0, 0, 0] for x in range(64)]
+
+    def __setitem__(self, key, value):
+        self.buffer[key[1] * self.DISPLAY_SIZE + key[0]] = clamp_rgb(value)
+
+    def __getitem__(self, key):
+        return self.buffer[key[1] * self.DISPLAY_SIZE + key[0]]
+
+    def draw(self):
+        hat.set_pixels(self.buffer)
+
 
 if __name__ == '__main__':
     main()
